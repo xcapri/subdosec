ae7d65c47cba492cc7113e727392f2e85ef37bee
xcapri <asarhasyim@gmail.com>
2024-07-19 19:13:40 +0700

    Fix : installer

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages, os
-
+from setuptools import setup, find_packages
 
 setup(
     name='Subdosec',
@@ -8,7 +7,8 @@ setup(
     author_email='N/A',
     url='https://github.com/xcapri/subdosec',
     version='0.2',
-    package_data={"subdosec_": ["config/*.env"]},
+    package_data={"subdosec_": ["config/*"]},
+    include_package_data=True,
     packages=find_packages(),
     install_requires=[
         'requests',
